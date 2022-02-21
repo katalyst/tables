244231b83e3700fdde4dfcc247ba0d06544372d9
Stephen Nelson <stephen@katalyst.com.au>
2022-02-22 09:03:51 +1030

    Initial commit
    ```
    bundler gem katalyst-tables
    ```

---
+++ lib/katalyst/tables.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "tables/version"
+
+module Katalyst
+  module Tables
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
