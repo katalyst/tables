d6efdc2fea644e39ce111909805d32596caff261
Stephen Nelson <stephen@katalyst.com.au>
2023-09-15 23:07:18 +0930

    Fix component initialisation and subclass to support extensions
    We want to allow individual table components to mix in extensions to
    their row and cell components. Subclass components on load so that
    they can be extended in isolation without affecting other tables.

--- app/components/concerns/katalyst/tables/configurable_component.rb
+++ app/components/concerns/katalyst/tables/configurable_component.rb
@@ -14,17 +14,38 @@ module Katalyst
 
       class_methods do
         # Define a configurable sub-component.
-        def config_component(name, component_name: "#{name}_component", default: nil)
+        # Sub-components are cached on the table instance. We want to allow run
+        # time mixins for tables so that we can extend tables with cross-cutting
+        # concerns that affect multiple sub-components by including the concern
+        # into the top-level table class. We achieve this by subclassing the
+        # component as soon as it is created so that when a mixin is added to
+        # the table class, it can immediately retrieve and modify the
+        # sub-component class as well without needing to worry about affecting
+        # other tables.
+        def config_component(name, component_name: "#{name}_component", default: nil) # rubocop:disable Metrics/MethodLength
           config_accessor(name)
           config.public_send("#{name}=", default)
           define_method(component_name) do
-            instance_variable_get("@#{component_name}") if instance_variable_defined?("@#{component_name}")
+            return instance_variable_get("@#{component_name}") if instance_variable_defined?("@#{component_name}")
 
             klass     = config.public_send(name)
-            component = self.class.const_get(klass) if klass
+            component = klass ? self.class.const_get(klass) : nil
+
+            # subclass to allow table-specific extensions
+            if component
+              component = Class.new(component)
+              component.extend(HiddenSubcomponent)
+            end
+
             instance_variable_set("@#{component_name}", component) if component
           end
         end
+      end
+
+      # View Component uses `name` to resolve the template path, so we need to
+      # hide the subclass from the template resolver.
+      module HiddenSubcomponent
+        delegate :name, to: :superclass
       end
     end
   end
