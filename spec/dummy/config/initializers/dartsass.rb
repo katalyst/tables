9f59b0c6d505bc20746de1561f7dc5fc4fc50aec
Stephen Nelson <stephen@katalyst.com.au>
2024-08-05 09:12:28 +0930

    Churn: update dependencies

--- spec/dummy/config/initializers/dartsass.rb
+++ spec/dummy/config/initializers/dartsass.rb
@@ -4,4 +4,4 @@ Rails.application.config.dartsass.builds = {
   "application.scss" => "application.css"
 }
 
-Rails.application.config.dartsass.build_options = "--quiet-deps"
+Rails.application.config.dartsass.build_options << "--quiet-deps"
